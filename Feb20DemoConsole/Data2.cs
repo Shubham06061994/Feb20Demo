719db8c5041b8ecc18caa8f096e32b7e5d29b51a
Shubham Tiple <shubham06051994@gmail.com>
2023-02-20 08:03:14 -0500

    Added Data2file

---
+++ Feb20DemoConsole/Data2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feb20DemoConsole
+{
+    class Data2
+    {
+        static void MainData2(string[] args)
+        {
+            Console.WriteLine("MainData2");
+        }
+    }
+}
