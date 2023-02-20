5c95bd33a08592c2c7598990e7387e1e21f0d9b4
Shubham Tiple <shubham06051994@gmail.com>
2023-02-20 07:48:59 -0500

    Data1 File Added

---
+++ Feb20DemoConsole/Data1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feb20DemoConsole
+{
+    class Data1
+    {
+        static void MainData1(string[] args)
+        {
+            Console.WriteLine("MainData1");
+        }
+    }
+}
