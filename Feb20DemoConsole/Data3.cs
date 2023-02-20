e26b76f7add23f279c4dccc03323b5c942c92abe
Shubham Tiple <shubham06051994@gmail.com>
2023-02-20 08:10:42 -0500

    Data 3file added

---
+++ Feb20DemoConsole/Data3.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feb20DemoConsole
+{
+    class Data3
+    {
+        static void MainData3(string[] args)
+        {
+            Console.WriteLine("MainData3");
+        }
+    }
+}
